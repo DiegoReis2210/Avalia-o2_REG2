22d1f2005ac3e328478b2809d67d58c73a0ae832
DiegoReis05 <diego.reis_1406@usp.br>
2025-12-01 19:21:24 -0300

    Delete Importações.py

--- Importações.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import statsmodels.formula.api as smf
-import statsmodels.api as sm
-from statsmodels.discrete.discrete_model import NegativeBinomial
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, mean_absolute_error
