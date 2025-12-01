36f320b2ef1ffa583324fa060f0a91939b8d7100
DiegoReis05 <diego.reis_1406@usp.br>
2025-12-01 18:56:15 -0300

    Create Importações.py

---
+++ Importações.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+from statsmodels.discrete.discrete_model import NegativeBinomial
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error
